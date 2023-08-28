30c437677da7b092341c19996fcb261b865c7d42
amit <amit1339@gmail.com>
2023-08-28 11:38:41 +0300

    update

--- 07-functions.js
+++
@@ -1,8 +0,0 @@
- function subscribed() {
-    if (document.querySelector('.subButtom').innerHTML === 'Subscribed') {
-        document.querySelector('.subButtom').innerHTML = 'Subscribe'
-    }
-    else{
-        document.querySelector('.subButtom').innerHTML = 'Subscribed';
-    }
- }
